445538447f5ef43dab300cd5cfa067dd51d11ef8
Peter43522 <pa27chitoiu@gmail.com>
2026-02-08 13:31:43 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
+function handleNoClick() {
+  const yesButton = document.querySelector('.yes-button');
+
+  const currentSize = parseFloat(
+    window.getComputedStyle(yesButton).fontSize
+  );
+  yesButton.style.fontSize = `${currentSize * 1.4}px`;
+}
+
+function handleYesClick() {
+  alert("YAY 💖");
+}
 
